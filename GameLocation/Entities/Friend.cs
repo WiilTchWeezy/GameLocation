9e8fcbca52fc54444b1942d50e3436b99437260c
TarcisioVitor <tarcisiosouzabr@gmail.com>
2017-10-29 02:43:57 -0200

    Ajustes

---
+++ GameLocation/Entities/Friend.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ GameLocation/Entities/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public DateTime LocationDate { get; set; }
+        public DateTime? DevolutionDate { get; set; }
+        public int FriendId { get; set; }
+        public DateTime Friend { get; set; }
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+    }
+}
